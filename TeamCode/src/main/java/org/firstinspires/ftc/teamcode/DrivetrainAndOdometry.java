bd4213e258905e40f7de6199a3f16fa8c281c62e
DH12043 <davidholleman24@gmail.com>
2020-11-17 23:48:53 -0600

    Created DrivetrainAndOdometry program to use drive, collect odomety data, and collect IMU data as well as set up for a shooter

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DrivetrainAndOdometry.java
@@ -0,0 +1,356 @@
+package org.firstinspires.ftc.teamcode;
+
+import android.os.SystemClock;
+import android.view.View;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+import org.firstinspires.ftc.robotcore.external.Func;
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
+import java.util.Locale;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import org.firstinspires.ftc.robotcore.internal.system.AppUtil;
+import com.qualcomm.robotcore.util.Range;
+import java.io.File;
+
+@TeleOp(name = "DrivetrainAndOdometry")
+public class DrivetrainAndOdometry extends OpMode {
+
+    //ODOMETRY/DRIVETRAIN/IMU VARIABLES AND DEVICES ------------------------------------------------
+
+    public static final double DEADZONE = 0.15;
+
+    BNO055IMU imu;
+    Orientation angles;
+
+    DcMotor verticalRight, verticalLeft, horizontal;
+
+    String verticalLeftEncoderName = "FrontLeft";
+    String verticalRightEncoderName = "BackRight";
+    String horizontalEncoderName = "FrontRight";
+
+    private double RobotXPosition;
+    private double RobotYPosition;
+    private double RobotRotation;
+
+    private double StartingXPosition;
+    private double StartingYPosition;
+    private double StartingRotation;
+
+    private double ScoringXPosition;
+    private double ScoringYPosition;
+    private double ScoringRotation;
+
+    private File startingXpositionFile = AppUtil.getInstance().getSettingsFile("startingXposition.txt");
+    private File startingYpositionFile = AppUtil.getInstance().getSettingsFile("startingYposition.txt");
+    private File startingθpositionFile = AppUtil.getInstance().getSettingsFile("startingθposition.txt");
+
+    final double COUNTS_PER_INCH = 307.699557;
+
+    private static final double DECELERATION_START_POINT = 24;
+    private static final double DECELERATION_ZERO_POINT = -6;
+    private static final double TURNING_DECELERATION_START_POINT = 100;
+    private static final double TURNING_DECELERATION_ZERO_POINT = -5;
+    private static final double X_SPEED_MULTIPLIER = 1;
+
+    private double lastDistanceToTarget = 0;
+    private double distanceToTarget;
+
+    OdometryGlobalCoordinatePosition globalPositionUpdate;
+    Thread positionThread;
+
+    //SHOOTER VARIABLES ----------------------------------------------------------------------------
+
+    private boolean firstPressShooterToggleButton = true;
+    private boolean shooterOn = false;
+
+    //GAMEPAD IMPUTS -------------------------------------------------------------------------------
+
+    private double movement_x;
+    private double movement_y;
+    private double movement_turn;
+
+    private boolean shooterToggleButton;
+
+   //MOTORS AND SERVOS -----------------------------------------------------------------------------
+
+    private DcMotor FrontRight;
+    private DcMotor FrontLeft;
+    private DcMotor BackRight;
+    private DcMotor BackLeft;
+
+    private DcMotor IntakeMotor;
+    private DcMotor ShooterMotor;
+
+    //LPS COUNTER ----------------------------------------------------------------------------------
+
+    private int loopCount;
+    private double loopStartTime;
+    private int loopsPerSecond;
+
+    private long lastUpdateTime = 0;
+
+    @Override
+    public void init() {
+        initializeDriveTrain();
+        initializeOdometry();
+        initializeIMU();
+        initializeShooter();
+        telemetry.addData("Status", "Init Complete");
+        telemetry.update();
+    }
+
+    @Override
+    public void start() {
+        startOdometry();
+        startIMU();
+        telemetry.addData("Status", "Odometry System has started");
+        telemetry.update();
+    }
+
+    @Override
+    public void loop() {
+        double currentTime = getRuntime();
+        loopCount++;
+        if (currentTime > loopStartTime + 5) {
+            loopsPerSecond = loopCount;
+            loopCount = 0;
+            loopStartTime = currentTime;
+        }
+        telemetry.addData("LPS: ", loopsPerSecond);
+
+        movement_y = DeadModifier(-gamepad1.left_stick_y);
+        movement_x = DeadModifier(gamepad1.left_stick_x);
+        movement_turn = DeadModifier(.75 * gamepad1.right_stick_x);
+
+        shooterToggleButton = gamepad1.a;
+
+        applyMovement();
+        checkOdometry();
+        runShooter();
+
+        telemetry.update();
+    }
+
+    //DRIVETRAIN -----------------------------------------------------------------------------------
+
+    private double DeadModifier(double joystickValue) {
+        if(joystickValue < DEADZONE && joystickValue > -DEADZONE)
+            return 0;
+        else {
+            return joystickValue;
+        }
+    }
+
+    private void initializeDriveTrain() {
+        FrontRight = hardwareMap.dcMotor.get("FrontRight");
+        FrontLeft = hardwareMap.dcMotor.get("FrontLeft");
+        BackRight = hardwareMap.dcMotor.get("BackRight");
+        BackLeft = hardwareMap.dcMotor.get("BackLeft");
+        FrontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        FrontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        BackRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        BackLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+
+    private void applyMovement() {
+        long currTime = SystemClock.uptimeMillis();
+        if(currTime - lastUpdateTime < 16){
+            return;
+        }
+        lastUpdateTime = currTime;
+
+        double fl_power_raw = movement_y+movement_turn+movement_x;
+        double bl_power_raw = movement_y+movement_turn-movement_x;
+        double br_power_raw = -movement_y+movement_turn+movement_x;
+        double fr_power_raw = -movement_y+movement_turn-movement_x;
+
+        //find the maximum of the powers
+        double maxRawPower = Math.abs(fl_power_raw);
+        if(Math.abs(bl_power_raw) > maxRawPower){ maxRawPower = Math.abs(bl_power_raw);}
+        if(Math.abs(br_power_raw) > maxRawPower){ maxRawPower = Math.abs(br_power_raw);}
+        if(Math.abs(fr_power_raw) > maxRawPower){ maxRawPower = Math.abs(fr_power_raw);}
+
+        //if the maximum is greater than 1, scale all the powers down to preserve the shape
+        double scaleDownAmount = 1.0;
+        if(maxRawPower > 1.0){
+            //when max power is multiplied by this ratio, it will be 1.0, and others less
+            scaleDownAmount = 1.0/maxRawPower;
+        }
+        fl_power_raw *= scaleDownAmount;
+        bl_power_raw *= scaleDownAmount;
+        br_power_raw *= scaleDownAmount;
+        fr_power_raw *= scaleDownAmount;
+
+        //now we can set the powers ONLY IF THEY HAVE CHANGED TO AVOID SPAMMING USB COMMUNICATIONS
+        FrontLeft.setPower(fl_power_raw);
+        BackLeft.setPower(-bl_power_raw);
+        BackRight.setPower(br_power_raw);
+        FrontRight.setPower(fr_power_raw);
+    }
+
+    //ODOMETRY -------------------------------------------------------------------------------------
+
+    private void initializeOdometry() {
+        verticalLeft = hardwareMap.dcMotor.get(verticalLeftEncoderName);
+        verticalRight = hardwareMap.dcMotor.get(verticalRightEncoderName);
+        horizontal = hardwareMap.dcMotor.get(horizontalEncoderName);
+
+        verticalRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        verticalLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        horizontal.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        //These values also affect the drive motors so we also reversed FrontRight
+        verticalLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        verticalRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        horizontal.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        FrontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        verticalRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        verticalLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        horizontal.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        //StartingXPosition = Double.parseDouble(ReadWriteFile.readFile(startingXpositionFile).trim());
+        //StartingYPosition = Double.parseDouble(ReadWriteFile.readFile(startingYpositionFile).trim());
+        //StartingRotation = Double.parseDouble(ReadWriteFile.readFile(startingθpositionFile).trim());
+        StartingXPosition = 0;
+        StartingYPosition = 0;
+        StartingRotation = 0;
+
+    }
+
+    private void startOdometry() {
+        globalPositionUpdate = new OdometryGlobalCoordinatePosition(verticalLeft, verticalRight, horizontal, COUNTS_PER_INCH, 25);
+        positionThread = new Thread(globalPositionUpdate);
+        positionThread.start();
+
+        globalPositionUpdate.reverseRightEncoder();
+    }
+
+    private void checkOdometry() {
+        RobotXPosition = (globalPositionUpdate.returnXCoordinate() / COUNTS_PER_INCH) + StartingXPosition;
+        RobotYPosition = (globalPositionUpdate.returnYCoordinate() / COUNTS_PER_INCH) + StartingYPosition;
+        RobotRotation = (globalPositionUpdate.returnOrientation()) + StartingRotation;
+
+        if (RobotRotation < 0){
+            RobotRotation += 360;
+        }
+
+        telemetry.addData("X", (Math.round (10*RobotXPosition) / 10));
+        telemetry.addData("Y", (Math.round (10*RobotYPosition) / 10));
+        telemetry.addData("θ", (Math.round (10*RobotRotation) / 10));
+
+        telemetry.addData("Vertical Left Encoder", verticalLeft.getCurrentPosition());
+        telemetry.addData("Vertical Right Encoder", verticalRight.getCurrentPosition());
+        telemetry.addData("Horizontal Encoder", horizontal.getCurrentPosition());
+
+        telemetry.addData("Thread Active", positionThread.isAlive());
+    }
+
+    //IMU ------------------------------------------------------------------------------------------
+
+    private void initializeIMU() {
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+        parameters.loggingEnabled      = true;
+        parameters.loggingTag          = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+
+        // Set up our telemetry dashboard
+        composeTelemetry();
+    }
+
+    private void startIMU() {
+        imu.startAccelerationIntegration(new Position(), new Velocity(), 1000);
+    }
+
+    void composeTelemetry() {
+
+        // At the beginning of each telemetry update, grab a bunch of data
+        // from the IMU that we will then display in separate lines.
+        telemetry.addAction(new Runnable() { @Override public void run()
+        {
+            // Acquiring the angles is relatively expensive; we don't want
+            // to do that in each of the three items that need that info, as that's
+            // three times the necessary expense.
+            angles   = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+        }
+        });
+
+        telemetry.addLine()
+                .addData("heading", new Func<String>() {
+                    @Override public String value() {
+                        return /*formatAngle(angles.angleUnit, */String.format("%.2f", angles.firstAngle)/*)*/;
+                    }
+                })
+                .addData("roll", new Func<String>() {
+                    @Override public String value() {
+                        return formatAngle(angles.angleUnit, angles.secondAngle);
+                    }
+                })
+                .addData("pitch", new Func<String>() {
+                    @Override public String value() {
+                        return formatAngle(angles.angleUnit, angles.thirdAngle);
+                    }
+                });
+    }
+
+    String formatAngle(AngleUnit angleUnit, double angle) {
+        if (angle < 0){
+            angle += 2*Math.PI;
+        }
+
+        return formatDegrees(AngleUnit.DEGREES.fromUnit(angleUnit, angle));
+    }
+
+    String formatDegrees(double degrees){
+        return String.format(Locale.getDefault(), "%.1f", AngleUnit.DEGREES.normalize(degrees));
+    }
+
+    //SHOOTER --------------------------------------------------------------------------------------
+
+    private void initializeShooter() {
+        ShooterMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        ShooterMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    private void runShooter() {
+        if (shooterToggleButton) {
+            if (firstPressShooterToggleButton) {
+                if (shooterOn) {
+                    ShooterMotor.setPower(0);
+                }
+                else {
+                    ShooterMotor.setPower(1);
+                }
+                firstPressShooterToggleButton = false;
+            }
+        }
+        else {
+            firstPressShooterToggleButton = true;
+        }
+    }
+
+
+    //INTAKE ---------------------------------------------------------------------------------------
+
+
+}
